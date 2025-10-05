bb39840d073bd65373b92fcacdc159b9f71ad23e
VinayNoogler000 <vinaytambey000@gmail.com>
2025-10-05 21:43:37 +0530

    Fix the error with 'robots.txt' not getting copies to the build/dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    createHtmlPlugin(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'robots.txt', // Path to your robots.txt file
+          dest: '', // Copy to the root of the dist folder
+        },
+      ]
+    }),
+    react()],
   optimizeDeps: {
     include: ['react-scroll', 'recharts'],
   },
